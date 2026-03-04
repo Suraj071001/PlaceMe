0c8db703ca570d1b657c92e0df9509691f2f6f0a
Jay patwal <jaypatwal102@gmail.com>
2026-03-04 23:08:00 +0530

    initialized server

---
+++ apps/http-server/src/app.ts
@@ -0,0 +1,8 @@
+import express from "express";
+import cors from "cors";
+const app = express();
+
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "hello world!" });
+});
+export default app;
