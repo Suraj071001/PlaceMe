8a40c5540aef1ccd0cd1e6d22a62cb38fbd762d8
Jay patwal <jaypatwal102@gmail.com>
2026-03-05 16:08:18 +0530

    jay/rbac

--- apps/http-server/src/modules/auth/dao.ts
+++ apps/http-server/src/modules/auth/dao.ts
@@ -1,7 +1,20 @@
 import client from "@repo/db/index";
 
 export const findUserByEmail = async (email: string) => {
-  return await client.user.findUnique({ where: { email } });
+  return await client.user.findUnique({
+    where: { email },
+    include: {
+      role: {
+        include: {
+          permissions: {
+            include: {
+              permission: true,
+            },
+          },
+        },
+      },
+    },
+  });
 };
 
 export const createUser = async (data: {
@@ -9,7 +22,26 @@ export const createUser = async (data: {
   password?: string;
   firstName?: string;
   lastName?: string;
-  role?: any;
 }) => {
-  return await client.user.create({ data: data as any });
+  const defaultRole = await client.role.findFirst({
+    where: { isDefault: true },
+  });
+
+  return await client.user.create({
+    data: {
+      ...data,
+      roleId: defaultRole?.id,
+    } as any,
+    include: {
+      role: {
+        include: {
+          permissions: {
+            include: {
+              permission: true,
+            },
+          },
+        },
+      },
+    },
+  });
 };
